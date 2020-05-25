aa22146c127b3017de012b6c2f88051c78f04791
natelol <ding184753928@126.com>
2020-05-26 07:13:33 +0800

    Update onekeyr2s.sh

--- onekeyr2s.sh
+++ onekeyr2s.sh
@@ -4,10 +4,10 @@ bash 01_get_ready.sh
 cd openwrt
 cp -r ../SCRIPTS/. ./
 bash 02_prepare_package.sh
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 bash 03_convert_translation.sh
 bash 04_remove_upx.sh
-mv ../SEED/config.seed .config
+rm -f .config
+cp ../SEED/config.seed .config
 make defconfig
 make download -j10
 chmod -R 755 ./
