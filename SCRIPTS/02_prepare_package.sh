40aef249b356cce44c1d7d0b0ed21ca89938c4b5
natelol <ding184753928@126.com>
2020-05-27 23:03:15 +0800

    add back in appfilter

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -100,7 +100,7 @@ svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network
 #svn co https://github.com/pymumu/smartdns/trunk/package/openwrt package/new/smartdns/smartdns
 #svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ntlf9t/luci-app-smartdns package/new/smartdns/luci-app-smartdns
 #上网APP过滤
-# git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
+git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/new/OpenAppFilter
 #FullCone补丁
 # FullCone 
 git clone -b master --single-branch https://github.com/QiuSimons/openwrt-fullconenat package/fullconenat
